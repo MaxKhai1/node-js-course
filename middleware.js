13b429fe7d73b0fae2ba965b3cd74f0d68ee7128
MaxKhai1 <m.akchurin@student.csn.khai.edu>
2023-12-13 02:25:25 +0200

    add express router, middleware, error handling

---
+++ middleware.js
@@ -0,0 +1,25 @@
+// middleware.js
+
+// A simple middleware function that logs the request details
+function logRequest(req, res, next) {
+    console.log(`Received a ${req.method} request to ${req.url}`);
+    next(); // Call the next middleware function in the chain
+ }
+ 
+ function blockSpecialBrand(req, res, next) {
+    if (req.params.brand === 'Brand C') {
+        res.status(403).send('Unavailable Brand');
+    } else {
+        next();
+    }
+ }
+
+ function blockSpecialId(req, res, next) {
+    if (req.params.id === '3') {
+        res.status(403).send('Unavailable product id');
+    } else {
+        next();
+    }
+ }
+ 
+ module.exports = { logRequest, blockSpecialBrand, blockSpecialId };
